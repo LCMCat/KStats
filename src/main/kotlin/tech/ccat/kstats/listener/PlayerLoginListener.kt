4df086a93c18b77be930b59e7f8eb2b226ef3e71
CMCat <1624498190@qq.com>
2025-12-20 15:10:32 +0800

    fix maxhealth issue

--- src/main/kotlin/tech/ccat/kstats/listener/PlayerLoginListener.kt
+++ src/main/kotlin/tech/ccat/kstats/listener/PlayerLoginListener.kt
@@ -1,10 +1,12 @@
 package tech.ccat.kstats.listener
 
+import org.bukkit.attribute.Attribute
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import tech.ccat.kstats.KStats
+import tech.ccat.kstats.model.StatType
 
 class PlayerLoginListener : Listener {
     private val plugin = KStats.instance
@@ -12,8 +14,10 @@ class PlayerLoginListener : Listener {
     @EventHandler
     fun onPlayerJoin(event: PlayerJoinEvent) {
         // 初始化并更新属性
-        plugin.statManager.initPlayerStats(event.player)
-        plugin.statManager.updateStats(event.player)
+        val player = event.player
+        plugin.statManager.initPlayerStats(player)
+        plugin.statManager.updateStats(player)
+        player.getAttribute(Attribute.MAX_HEALTH)?.baseValue = plugin.statManager.getStat(player, StatType.HEALTH)
     }
 
     @EventHandler
